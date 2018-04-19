62a64804a7456e81feae19295a2a50e3072fdaa2
Daniel Paschke <paschdan@gmail.com>
2018-04-19 16:10:33 +0200

    feat(backend): serve graphql playground on /
    serve graphql playground from
    `https://github.com/graphcool/graphql-playground`
    
    create-backend-boilerplate#4
    
    create-backend-boilerplate#4

--- backend/main.go
+++ backend/main.go
@@ -3,19 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
-	"strings"
+
+	"github.com/natascore/git-review/backend/handler"
 )
 
-func sayHello(w http.ResponseWriter, r *http.Request) {
-	message := r.URL.Path
-	message = strings.TrimPrefix(message, "/")
-	message = "Hello " + message
-	w.Write([]byte(message))
-}
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", sayHello)
+	mux.Handle("/", handler.Playground{})
 
 	log.Printf("About to listen on :8080: Go to http://127.0.0.1:8080/")
 	err := http.ListenAndServe(":8080", mux)
