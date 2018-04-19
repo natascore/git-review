93981a013dcbfd4dd976907d2604539050224a0c
Daniel Paschke <paschdan@gmail.com>
2018-04-19 13:54:52 +0200

    feat(backend): add initial backend
    create-backend-boilerplate#4

---
+++ backend/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strings"
+)
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Path
+	message = strings.TrimPrefix(message, "/")
+	message = "Hello " + message
+	w.Write([]byte(message))
+}
+func main() {
+	http.HandleFunc("/", sayHello)
+	log.Printf("About to listen on :8080: Go to http://127.0.0.1:8080/")
+	err := http.ListenAndServe(":8080", nil)
+	log.Fatal(err)
+}
