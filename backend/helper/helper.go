9b4a9f3d6b2cf9807cefe86187c5fd821daa9f8a
Daniel Paschke <paschdan@gmail.com>
2018-04-27 13:46:50 +0200

    perf(backend): better error handling
    do not exit upon error, instead return error in graphql

--- backend/helper/helper.go
+++ backend/helper/helper.go
@@ -2,7 +2,6 @@ package helper
 
 import (
 	"fmt"
-	"os"
 )
 
 // CheckIfError should be used to naively panics if an error is not nil.
@@ -11,8 +10,7 @@ func CheckIfError(err error) {
 		return
 	}
 
-	fmt.Printf("\x1b[31;1m%s\x1b[0m\n", fmt.Sprintf("error: %s", err))
-	os.Exit(1)
+	panic(err)
 }
 
 // Info should be used to describe the example commands that are about to run.
