8ca9b68818c9ce2ed5bdbd50906fe048293b8934
Daniel Paschke <paschdan@gmail.com>
2018-04-27 13:45:11 +0200

    feat(backend): add pagination to history

--- backend/resolver/history_query.go
+++ backend/resolver/history_query.go
@@ -3,30 +3,50 @@ package resolver
 import (
 	helper "github.com/natascore/git-review/backend/helper"
 	git "gopkg.in/src-d/go-git.v4"
-	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	"gopkg.in/src-d/go-git.v4/plumbing"
 )
 
 // History QueryResolver for git log
-func (r *Resolver) History() *[]*CommitResolver {
+func (r *Resolver) History(args struct {
+	First int32
+	After *string
+}) *[]*CommitResolver {
 
 	repo, err := git.PlainOpen("../")
 	helper.CheckIfError(err)
 
-	// ... retrieving the HEAD reference
-	ref, err := repo.Head()
-	helper.CheckIfError(err)
-
-	// ... retrieves the commit history
-	cIter, err := repo.Log(&git.LogOptions{From: ref.Hash()})
-	helper.CheckIfError(err)
-
+	var hash plumbing.Hash
 	var l []*CommitResolver
 
-	err = cIter.ForEach(func(c *object.Commit) error {
-		l = append(l, &CommitResolver{c, repo})
-		return nil
-	})
-	helper.CheckIfError(err)
-	return &l
+	if args.After != nil {
+		curentHash := plumbing.NewHash(string(*args.After))
+		commit, err := repo.CommitObject(curentHash)
+		if err != nil {
+			panic("commit not found")
+		}
+		parent, err := commit.Parent(0)
+		// if it is the initial commit we return an empty list
+		if err != nil {
+			return &l
+		}
+		hash = parent.Hash
+	} else {
+		// ... retrieving the HEAD reference
+		ref, err := repo.Head()
+		helper.CheckIfError(err)
+		hash = ref.Hash()
+	}
 
+	// ... retrieves the commit history
+	cIter, err := repo.Log(&git.LogOptions{From: hash})
+	helper.CheckIfError(err)
+
+	for i := 0; i < int(args.First); i++ {
+		c, err := cIter.Next()
+		if err != nil {
+			break
+		}
+		l = append(l, &CommitResolver{c, repo})
+	}
+	return &l
 }
