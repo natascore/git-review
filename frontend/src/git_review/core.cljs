28d69f40c024aca1a2e2a817da543d3339041881
Oliver Esser <oliver.esser@campus.tu-berlin.de>
2018-04-28 23:20:21 +0200

    feat(frontend): load commit history from backend api

--- frontend/src/git_review/core.cljs
+++ frontend/src/git_review/core.cljs
@@ -1,55 +1,15 @@
 (ns git-review.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [clojure.string :as string]
             [git-review.crypt :as crypt]
+            [cljs-http.client :as http]
+            [cljs.core.async :as async]
             [goog.dom :as dom]
             [rum.core :as rum]))
 
 (enable-console-print!)
 
-(def history-fixture [{:hash "4b64c830fad432bbc465f2fb5bb2f63bac6a09db",
-                       :author {:name "Oliver Esser", :email "oliver.esser@actano.de"},
-                       :message "chore(backend): update Gopck.lock file",
-                       :date "2018-04-25T07:25:21Z"}
-                      {:hash "203167f101360eea5fb73a72a54a1a8eb556eb10",
-                       :author {:name "Daniel Paschke", :email "paschdan@gmail.com"},
-                       :message "feat(backend): add committer attribute",
-                       :date "2018-04-23T07:57:42Z"}
-                      {:hash "8f210b18161bb98262ce49a757c08377a30c4877",
-                       :author {:name "Daniel Paschke", :email "paschdan@gmail.com"},
-                       :message "feat(backend): add author to `commit`",
-                       :date "2018-04-23T07:50:47Z"}
-                      {:hash "34f72faefa32c651d907b8a23e9adcc6782b99d0",
-                       :author {:name "Daniel Paschke", :email "paschdan@gmail.com"},
-                       :message "feat(backend): add date to commit type",
-                       :date "2018-04-23T07:34:32Z"}
-                      {:hash "7a3130ee3052ad9e92a3004efd6012e00b82e4df",
-                       :author {:name "Daniel Paschke", :email "paschdan@gmail.com"},
-                       :message "feat(backend): adds message to commit type",
-                       :date "2018-04-22T11:44:02Z"}
-                      {:hash "37596210681ac5a8ec3c30103f05a38e362ba6b5",
-                       :author {:name "Daniel Paschke", :email "paschdan@gmail.com"},
-                       :message "feat(backend): retrieve history from current git dir",
-                       :date "2018-04-22T11:42:40Z"}
-                      {:hash "63c7a048c04b7a15d22cae134ee0c95973df9f7f",
-                       :author {:name "Daniel Paschke", :email "paschdan@gmail.com"},
-                       :message
-                       "feat(backend): adds helper for checking errors and logging",
-                       :date "2018-04-22T11:38:09Z"}
-                      {:hash "1ad77d840a6b6fe3c53894204577372ec01ace18",
-                       :author {:name "Daniel Paschke", :email "paschdan@gmail.com"},
-                       :message
-                       "feat(backend): create initial schema for retrieving commitlog",
-                       :date "2018-04-21T07:08:34Z"}
-                      {:hash "0e6d346ccf7b2c9db4430291880a9d4baeb9e2e3",
-                       :author {:name "Daniel Paschke", :email "paschdan@gmail.com"},
-                       :message "docs(backend): adds resolver comment for helloWorld",
-                       :date "2018-04-21T06:50:01Z"}
-                      {:hash "b2504fa399c2046ff688ca9f91442591c1d6a7a7",
-                       :author {:name "Daniel Paschke", :email "paschdan@gmail.com"},
-                       :message "style(backend): remove superfluous comment",
-                       :date "2018-04-20T10:07:10Z"}])
-
-(defonce app-state (atom {:history history-fixture}))
+(defonce app-state (atom {:history []}))
 
 (defn format-date [isodate]
   (-> isodate
@@ -78,7 +38,40 @@
       [:.commit-hash hash]
       [:.commit-date (format-date date)]]]))
 
-(rum/defc commit-list < rum/reactive []
+
+(defn summarize-commit [commit]
+  (let [{:keys [message]} commit]
+    (assoc commit :message (first (string/split message #"\n")))))
+
+(defn summarize-history [history]
+  (map summarize-commit history))
+
+(defn update-state [c state]
+  (go
+    (let [{:keys [status body]} (async/<! c)
+          history (get-in body [:data :history])]
+      (reset! state {:history (summarize-history history)}))))
+
+(def api-query
+  (str "query {"
+  "history(first: 10){"
+  "date "
+  "hash "
+  "message "
+  "author { name email }"
+  "}"
+  "}"))
+
+(defn load-from-api []
+  (-> (http/post "http://localhost:8080/graphql"
+            {:with-credentials? false
+             :json-params {:query api-query}})
+      (update-state app-state)))
+
+(rum/defc commit-list <
+  rum/reactive
+  {:will-mount (fn [state] (load-from-api) state)}
+  []
   [:.commit-list
    (let [history (:history (rum/react app-state))]
      (if (empty? history)
