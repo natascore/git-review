c1926848e2f2f5c8d69c06437e7e8f4d5f8e6dfb
Oliver Esser <oliver.esser@campus.tu-berlin.de>
2018-04-28 22:50:29 +0200

    feat(backend): allow cross origin requests

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+  "github.com/rs/cors"
 	"github.com/natascore/git-review/backend/handler"
 )
 
@@ -13,7 +14,9 @@ func main() {
 	mux.Handle("/graphql", handler.GraphQLHandler{})
 	mux.Handle("/", handler.Playground{})
 
+  handler := cors.Default().Handler(mux)
+
 	log.Printf("About to listen on :8080: Go to http://127.0.0.1:8080/")
-	err := http.ListenAndServe(":8080", mux)
+	err := http.ListenAndServe(":8080", handler)
 	log.Fatal(err)
 }
